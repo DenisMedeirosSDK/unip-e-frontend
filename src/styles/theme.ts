2a7fa6c328352e8aa9da4a8c89494349cb745345
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-09-15 21:34:57 -0300

    feat: create home page

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: '#FFFF',
+    background_semi: '#DCE2E5',
+    header: '#075EDB',
+    text: '#343A40',
+    text_light: '#FFF',
+    unip_color: '#FFF100',
+  },
+};
+
+export default theme;
