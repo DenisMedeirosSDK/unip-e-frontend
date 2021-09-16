eeb750afb24baa925fb3f3e24bd57f857b4b29a4
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-09-16 08:45:53 -0300

    feat: create intershipForm component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Container, Header, Content, Title, Divider } from '../styles/Home';
 import UnipLogoSvg from '../assets/logo-unip.svg';
 import { StudentForm } from '../components/StudentForm';
 import { CompanyForm } from '../components/CompanyForm';
+import { InternshipForm } from '../components/InternshipForm';
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
         <StudentForm />
         <Divider />
         <CompanyForm />
+        <Divider />
+        <InternshipForm />
       </Content>
     </Container>
   );
